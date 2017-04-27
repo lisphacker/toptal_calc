b979d89f60df9a8f5161ff3100b45416c2ec9b9a
Gautham Ganapathy <gautham@lisphacker.org>
2017-04-27 12:27:02 +0100

    New polynomial term structure

--- src/Math.hs
+++ src/Math.hs
@@ -4,20 +4,22 @@ import ExprTree
 import Parser
 
 -- | Definition of a polynomial term.
-data PolyTerm = PolyTerm { coeff :: Float
-                         , order :: Int
-                         }
+data PolyTerm = PolyTerm [(Int, Float)]
 
 instance Show PolyTerm where
-  show (PolyTerm c o) = show c ++ "x^" ++ show o
+  show (PolyTerm xs) = "p<" ++ show' xs ++ ">"
+    where show' [] = ""
+          show' ((o,c):xs) =
+            let s = show c ++ (if o == 0 then "0" else "x" ++ (if o == 1 then "" else "^" ++ show o))
+            in s ++ (if null xs then "" else " + " ++ show' xs)
 
 -- | Definition of a polynomial expression tree.
 type PolyExpr = ExprTree PolyTerm
 
 convertTreeToPolyTree :: Expr -> PolyExpr
 convertTreeToPolyTree = fmap convert'
-  where convert' (Numeric v)  = PolyTerm v 0
-        convert' (Variable _) = PolyTerm 1 1
+  where convert' (Numeric v)  = PolyTerm [(0, 1)]
+        convert' (Variable _) = PolyTerm [(1, 1)]
 
 evaluateExpr (ExprValue (Numeric v)) = v
 evaluateExpr (ExprOp op e1 e2) = let v1 = evaluateExpr e1
@@ -32,7 +34,9 @@ evaluateExpr (ExprFn fn e) = let v = evaluateExpr e
   where eval' Log v = (log v) / (log 10)
         eval' Ln v  = log v
 
-solveLinEq = id
+solveLinEq (ExprOp Eq lhs rhs) =
+  let expr = ExprOp Sub lhs rhs
+  in expr
 
 processMathExpression s =
   case parse s of
@@ -41,7 +45,7 @@ processMathExpression s =
       case expr of
         ExprOp Eq _ _ -> show $ solveLinEq $ convertTreeToPolyTree expr
         _             -> show $ evaluateExpr expr
-  
+
 
 ------------------------------------------------------------
 
