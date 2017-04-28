ff23ea98b644d4cc0f0e253261cbed5a537a6bcf
Gautham Ganapathy <gautham@lisphacker.org>
2017-04-28 01:17:37 +0100

    Added haskeline

--- prog-src/Main.hs
+++ prog-src/Main.hs
@@ -2,13 +2,25 @@ module Main where
 
 import Math
 import System.IO
+import System.Console.Haskeline
 
 prompt = "> "
 
 main :: IO ()
+{-
 main = do
-  putStr prompt
-  hFlush stdout
-  input <- getLine
+  input <- getInputLine prompt
   putStrLn $ processMathExpression input
   main
+-}
+main = runInputT defaultSettings loop
+  where
+    loop :: InputT IO ()
+    loop = do
+      mInput <- getInputLine prompt
+      case mInput of
+        Just "exit" -> return()
+        Just input  -> do outputStrLn $ processMathExpression input
+                          loop
+        Nothing     -> loop
+        
