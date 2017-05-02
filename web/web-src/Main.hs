f6f4e025cc67c1f1057a079b1cf97bf534017400
Gautham Ganapathy <gautham@lisphacker.org>
2017-05-02 02:44:56 +0100

    Early bootstrap css integration

--- web/web-src/Main.hs
+++ web/web-src/Main.hs
@@ -1,20 +1,31 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, RecursiveDo, ScopedTypeVariables, FlexibleContexts, TypeFamilies, ConstraintKinds, TemplateHaskell #-}
 import Reflex.Dom
 import Math
 import Data.Text
+import Data.FileEmbed
+import Data.Map.Strict
 
+{-
 main = mainWidget $ el "div" $ do
   t <- textInput def
   let (errDynText, resultDynText) = splitDynPure  $ fmap runMath $ _textInput_value t
   el "p" $ dynText errDynText
   el "p" $ dynText resultDynText
+-}
 
 
+main :: IO ()
+main = mainWidgetWithCss $(embedFile "static/css/style.css") $ el "div" $ do
+  t <- elClass "div" "input-group" $ do
+    elClass "span" "input-group-addon" $ text "Input"
+    textInputWithClass "form-control"
+  let (errDynText, resultDynText) = splitDynPure  $ fmap runMath $ _textInput_value t
+  elClass "p" "text-danger" $ dynText errDynText
+  el "p" $ dynText resultDynText
 
-
-
---main :: IO ()
---main = mainWidgetWithCss $(embedFile "style.css") todoMVC
+textInputWithClass cls = let cfg = constDyn $ fromList [("class", cls)]
+                         in textInput $ def & textInputConfig_attributes .~ cfg
+                            
 
 noTxt = pack ""
 runMath :: Text -> (Text, Text)
@@ -22,3 +33,4 @@ runMath input = case processMathExpression (unpack input) of
                  Left (MathError msg) -> (pack $ if msg == "Parse error: Empty input" then "" else msg, noTxt)
                  Right v              -> (noTxt, pack $ v')
                    where v' = if Prelude.length v == 1 then (show . Prelude.head) v else show v
+
