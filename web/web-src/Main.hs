71232ee5c7838f6940d4566d99c1e1e253ba6a27
Gautham Ganapathy <gautham@lisphacker.org>
2017-05-02 01:53:16 +0100

    Basic working web version

--- web/web-src/Main.hs
+++ web/web-src/Main.hs
@@ -5,4 +5,20 @@ import Data.Text
 
 main = mainWidget $ el "div" $ do
   t <- textInput def
-  dynText $ fmap (pack . processMathExpression . unpack) $ _textInput_value t
+  let (errDynText, resultDynText) = splitDynPure  $ fmap runMath $ _textInput_value t
+  el "p" $ dynText errDynText
+  el "p" $ dynText resultDynText
+
+
+
+
+
+--main :: IO ()
+--main = mainWidgetWithCss $(embedFile "style.css") todoMVC
+
+noTxt = pack ""
+runMath :: Text -> (Text, Text)
+runMath input = case processMathExpression (unpack input) of
+                 Left (MathError msg) -> (pack $ if msg == "Parse error: Empty input" then "" else msg, noTxt)
+                 Right v              -> (noTxt, pack $ v')
+                   where v' = if Prelude.length v == 1 then (show . Prelude.head) v else show v
